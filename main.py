04f3a50e3ded20044d8dfde1f61d8ed0bd277175
tharnnapat <ku.tharnnapat_st@tni.ac.th>
2013-09-03 18:10:11 +0700

    18.10

--- main.py
+++ main.py
@@ -42,6 +42,8 @@ class CalendarView(webapp2.RequestHandler):
 	@decorator.oauth_required
 	def get(self):
 
+
+
 		http = decorator.http()
 		events = service.events().list(calendarId='primary').execute(http=http)
 		year = []
@@ -52,11 +54,45 @@ class CalendarView(webapp2.RequestHandler):
 		end_time_hour = []
 		end_time_minute = []
 		allday_time = []
+		recurrence = []
+		recurrence2 = []
+		title = []
 
-	        for event in events['items']:
-	            if 'dateTime' in event['start']:
-					dateTime = event['start']['dateTime']
-					dateTime2 = event['end']['dateTime']
+		for event in events['items']:
+			dateTime = event['start']['dateTime']
+			dateTime2 = event['end']['dateTime']
+			title.append(event['summary'])
+			if 'timeZone' in event['start']:
+				if 'recurrence' in event:
+					rec = event['recurrence']
+					recurrence.append(rec)
+					year.append(dateTime[:4])
+					month.append(dateTime[5:-18])
+					date.append(dateTime[8:-15]) 	   
+					start_time_hour.append(00)
+					start_time_minute.append(00)
+					end_time_hour.append(00)
+					end_time_minute.append(00)
+					allday_time.append(1)
+					self.response.write("IF 2")
+					self.response.write("</br>")		
+
+				else:
+					recurrence.append(0)
+					year.append(dateTime[:4])
+					month.append(dateTime[5:-18])
+					date.append(dateTime[8:-15]) 	   
+					start_time_hour.append(00)
+					start_time_minute.append(00)
+					end_time_hour.append(00)
+					end_time_minute.append(00)
+					allday_time.append(1)		
+					self.response.write("ELSE 2")				
+					self.response.write("</br>")		
+			else:
+				if 'recurrence' in event:
+					rec = event['recurrence']
+					recurrence.append(rec)
 					year.append(dateTime[:4])
 					month.append(dateTime[5:-18])
 					date.append(dateTime[8:-15])
@@ -65,55 +101,56 @@ class CalendarView(webapp2.RequestHandler):
 					end_time_hour.append(dateTime2[11:-12])
 					end_time_minute.append(dateTime2[14:-9])
 					allday_time.append(0)
-				         		            		            		  	            		            	            	
-	            else:
-					dateTime = event['start']['date']
-					dateTime2 = event['end']['date']
+					self.response.write("IF 1")
+					self.response.write("</br>")											         
+				else:
+					recurrence.append(0)
 					year.append(dateTime[:4])
-					month.append(dateTime[5:-3])
-					date.append(dateTime[8:]) 	   
-					start_time_hour.append(00)
-					start_time_minute.append(00)
-					end_time_hour.append(00)
-					end_time_minute.append(00)
-					allday_time.append(1)
+					month.append(dateTime[5:-18])
+					date.append(dateTime[8:-15])
+					start_time_hour.append(dateTime[11:-12])
+					start_time_minute.append(dateTime[14:-9])
+					end_time_hour.append(dateTime2[11:-12])
+					end_time_minute.append(dateTime2[14:-9])
+					allday_time.append(0)
+					self.response.write("ELSE 1")					         		            		            		  	            		            	            	
+					self.response.write("</br>")	
 
 
-		for x in year:
-			self.response.write("Year :: ")			
-			self.response.write(x)
+		len_event = len(year)
+
+		for row in range(0,len(recurrence)):
+			if recurrence[row] != 0:
+				rec = str(recurrence[row])
+				x = list(str(recurrence[row]))
+				if x[28] == "'" :
+					recurrence2.append(int(rec[27:-2]))
+				else:			
+					recurrence2.append(int(rec[27:-2]))
+			else:
+				recurrence2.append(int(1))
+
+		self.response.write("RECCURRENCE 2")
+		self.response.write("</br>")
+
+		for row in range(0,len(recurrence)):
+			self.response.write(recurrence2[row])
 			self.response.write("</br>")
-		for y in month:
-			self.response.write("Month :: ")
-			self.response.write(y)
+
+		self.response.write("ALL DAY TIME")
+		for row in range(0,len(allday_time)):
+			self.response.write(allday_time[row])
 			self.response.write("</br>")
-		for z in date:
-			self.response.write("Date :: ")
-			self.response.write(z)
-			self.response.write("</br>")
-		for a in start_time_hour:
-			self.response.write("Start_time_hour :: ")
-			self.response.write(a)
-			self.response.write("</br>")
-		for b in start_time_minute:
-			self.response.write("Start_time_minute :: ")
-			self.response.write(b)
-			self.response.write("</br>")			
-		for c in end_time_hour:
-			self.response.write("End_time_hour :: ")
-			self.response.write(c)
-			self.response.write("</br>")
-		for d in end_time_minute:
-			self.response.write("End_time_minute  :: ")
-			self.response.write(d)
-			self.response.write("</br>")
-		for e in allday_time:
-			self.response.write("Type  :: ")
-			self.response.write(e)
-			self.response.write("</br>")	
+
+
+		self.response.write("<a href='/InsertCelendar'> Insert </a>")
+
+		self.response.write(len_event)
+
 
 		templates = {
 				'events' : events['items'],
+				'title' : title,	
 				'year' : year,
 				'month' : month,
 				'date' : date,	
@@ -122,6 +159,9 @@ class CalendarView(webapp2.RequestHandler):
 				'end_time_hour' : end_time_hour,	
 				'end_time_minute' : end_time_minute,	
 				'allday_time' : allday_time,	
+				'recurrence2' : recurrence2,	
+				'len_event' : len_event,
+
 			}
 
 		template = JINJA_ENVIRONMENT.get_template('course_regis.html')
@@ -131,11 +171,14 @@ class InsertCelendar(webapp2.RequestHandler):
     @decorator.oauth_required
     def get(self):
 
-		open_semester_year = '2013-09-05'
-		weekly_study = 3
+		open_semester_year = '2013-06-27' #Insert Date on Monday
+		weekly_study = 13
 
     	# course_code = self.request.get('');
     	# section = self.request.get('');
+
+		course_code = 'INT-101'
+		section = 1
 
 		today = datetime.datetime.now()
 		weekday = today.weekday()+2
@@ -144,8 +187,7 @@ class InsertCelendar(webapp2.RequestHandler):
 		month_num=int(open_semester_year[5:-3])
 		day_num =int(open_semester_year[8:])
 
-		course_code = 'INT-101'
-		section = 1
+
 
 		conn = rdbms.connect(instance=_INSTANCE_NAME, database='Prinya_Project')
 		cursor = conn.cursor()
@@ -187,55 +229,43 @@ class InsertCelendar(webapp2.RequestHandler):
 					if month_num==1 or month_num==3 or month_num==5 or month_num==7 or month_num==8 or month_num==10 or month_num==12 :
 						if day_num+d <= 31 :					
 							insert_day.append(day_num+d-2)
-							self.response.write("IF 1")
-							self.response.write("</br>")
 							month_num2.append(month_num)
-						# else:
-						# 	insert_day.append(day_num+d-33)
-						# 	month_num2.append(month_num+1)
-						# 	self.response.write("ELSE 1")
-						# 	self.response.write("</br>")													
+						else:
+							insert_day.append(day_num+d-33)
+							month_num2.append(month_num+1)												
 										
 					elif month_num==4 or month_num==6 or month_num==9 or month_num==11:
 						if day_num+d <= 30 :					
 							insert_day.append(day_num+d-2)
-							self.response.write("IF 2")
-							self.response.write("</br>")
 							month_num2.append(month_num)
-						# else:
-						# 	insert_day.append(day_num+d-34)
-						# 	month_num2.append(month_num+1)
-						# 	self.response.write(day_num)
-						# 	self.response.write("ELSE 2")
-						# 	self.response.write("</br>")													
+						else:
+							insert_day.append(day_num+d-32)
+							month_num2.append(month_num+1)
+							self.response.write(day_num)
+												
 					elif month_num==2:
 						if day_num+d <= 28 :					
 							insert_day.append(day_num+d-2)
-							self.response.write("IF 3")
-							self.response.write("</br>")
 							month_num2.append(month_num)
-						# else:
-							# insert_day.append(day_num+d-35)
-							# month_num2.append(month_num+1)
-							# self.response.write("ELSE 3")
-							# self.response.write("</br>")														
+						else:
+							insert_day.append(day_num+d-30)
+							month_num2.append(month_num+1)													
 		
 		for x in range(0,len(insert_day)):
-			insert_day2.append('2013-09-'+str(insert_day[x]))	
-			self.response.write("</br>")
+			insert_day2.append('2013-'+str(month_num2[x])+'-'+str(insert_day[x]))	
 
-		for x in range(0,len(insert_day)):
-			self.response.write("Insert Day 1 :: ")
-			self.response.write(insert_day[x])	
-			self.response.write("</br>")
-			self.response.write("Insert Day 2 :: ")
-			self.response.write(insert_day2[x])	
-			self.response.write("</br>")
+		# for x in range(0,len(insert_day)):
+		# 	self.response.write("Insert Day 1 :: ")
+		# 	self.response.write(insert_day[x])	
+		# 	self.response.write("</br>")
+		# 	self.response.write("Insert Day 2 :: ")
+		# 	self.response.write(insert_day2[x])	
+		# 	self.response.write("</br>")
 
-		for x in range(0,len(month_num2)):
-			self.response.write("Month NUM :: ")
-			self.response.write(month_num2[x])	
-			self.response.write("</br>")
+		# for x in range(0,len(month_num2)):
+		# 	self.response.write("Month NUM :: ")
+		# 	self.response.write(month_num2[x])	
+		# 	self.response.write("</br>")
 		
 
 
@@ -259,15 +289,17 @@ class InsertCelendar(webapp2.RequestHandler):
 
 			insert_event = service.events().insert(calendarId='primary',body=event).execute(http=http)
 
+		self.redirect('/')
+
 		self.response.write("Create event success  ")
 
 		self.response.write("<a href='/DeleteCalendar'> Delete </a>")
-
+		self.response.write("<a href='/'> Home </a>")
 class DeleteCalendar(webapp2.RequestHandler):
 	@decorator.oauth_required
 	def get(self):
 
-		name_get = 'A101'
+		name_get = 'B201'
 
 
 		http = decorator.http()
@@ -282,23 +314,23 @@ class DeleteCalendar(webapp2.RequestHandler):
 				ID_events.append(id_event)
 				name_events.append(name_event)
 
-		for x in range(0,len(ID_events)):
-			self.response.write(ID_events[x])	
-			self.response.write("</br>")
-			self.response.write(name_events[x])	
-			self.response.write("</br>")
+		# for x in range(0,len(ID_events)):
+		# 	self.response.write(ID_events[x])	
+		# 	self.response.write("</br>")
+		# 	self.response.write(name_events[x])	
+		# 	self.response.write("</br>")
 
 		for y in range(0,len(ID_events)):	
 			if name_get == name_events[y]:
 				http = decorator.http()
-				service.events().delete(calendarId='primary', eventId='%s').execute(http=http)%(ID_events[y])
+				service.events().delete(calendarId='primary', eventId=ID_events[y]).execute(http=http)
 
-		self.response.write("<a href='/InsertCelendar'> Insert </a>")
+		self.redirect('/')
 
 
 app = webapp2.WSGIApplication([
-    ('/', InsertCelendar),
-    # ('/CalendarView', CalendarView),
+    ('/', CalendarView),
+    ('/InsertCelendar', InsertCelendar),
     ('/DeleteCalendar', DeleteCalendar),
     (decorator.callback_path,decorator.callback_handler())
 ], debug=True)
